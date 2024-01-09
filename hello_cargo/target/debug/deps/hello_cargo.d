9e290103ffe1f260c6772cb3b7a74b228f9bb6b8
Pat <mediodia.patrick@gmail.com>
2024-01-09 15:23:12 +0800

    Added hello world to repo

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+E:\Desktop\Learning-Rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+E:\Desktop\Learning-Rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+E:\Desktop\Learning-Rust\hello_cargo\target\debug\hello_cargo.exe: E:\Desktop\Learning-Rust\hello_cargo\src\main.rs
